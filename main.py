98bcddfb0c0f348e4297328070b992d061398d32
michaldoman <mjd.domanski@gmail.com>
2023-06-19 17:46:10 +0200

    v4.5 - Board doc strings

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from SudokuSolver import SudokuSolver
-from tools import load_board
+from utils import load_board
 from sudoku_boards import *
 
+# Settings:
 SIDE = 100
 MARGIN = 50
 WIDTH = 9 * SIDE + 2 * MARGIN
@@ -15,10 +16,18 @@ class Board(Frame):
     """
     This class is the main frame of the application which holds the whole content.
     It contains functions which draw Sudoku board and all necessary components as well as
-    functions that allow the user to perform actions within the app. Sudoku board is divided
+    methods that allow the user to perform actions within the app. Sudoku board is divided
     into 9x9 cells, and cells are segregated by clusters, which are rows, columns and 3x3 squares.
     """
     def __init__(self, master):
+        """
+        Create canvas and add all necessary widgets to it.
+        Create all functionalities like key bindings, movement on the Sudoku grid and typing digits.
+        Create and segregate cells into clusters (necessary for the Solver).
+        Add buttons functionalities, and save action history for undoing.
+
+        :param master main window of the application (root), to which the Board instance is assigned.
+        """
         super().__init__(master)
         # Prepare canvas and widgets:
         self.pack(fill=BOTH)
@@ -84,6 +93,10 @@ class Board(Frame):
         self.canvas.bind("<Control-z>", lambda key_press: self.undo())
 
     def draw_grid(self):
+        """
+        Draw a 9x9 Sudoku grid on the canvas. Every third line starting from the first line
+        of the grid's rows and columns is bold.
+        """
         for line_nr in range(10):
             # change color and line width for every 3rd line:
             color = 'grey'
@@ -105,6 +118,10 @@ class Board(Frame):
             self.canvas.create_line(x1, y, x2, y, fill=color, width=line_width)
 
     def create_cells(self):
+        """
+        Create an instance of Cell class for every square of the grid and assign coordinates.
+        Append all the cells to the list (segregated by rows).
+        """
         for row_nr in range(9):
             row = []
 
@@ -120,6 +137,7 @@ class Board(Frame):
             self.cells.append(row)
 
     def create_columns(self):
+        """Reorganise cells into a list of columns."""
         for col_nr in range(len(self.cells)):
             column = []
             for row in self.cells:
@@ -128,6 +146,7 @@ class Board(Frame):
             self.columns.append(column)
 
     def create_squares(self):
+        """Reorganise cells into a list of 3x3 squares."""
         square_id = 0
         for square_x in range(0, 9, 3):  # for 0, 3 and 6
             for square_y in range(0, 9, 3):
@@ -141,6 +160,10 @@ class Board(Frame):
                 square_id += 1
 
     def choose_cell(self, click_coord):
+        """
+        Highlight and select cell as current with mouse click.
+        :param click_coord: window coordinates of LMB click
+        """
         x = click_coord.x
         y = click_coord.y
 
@@ -153,6 +176,14 @@ class Board(Frame):
                     break
 
     def choose_digit(self, key_press):
+        """
+        Display a digit typed by the user if the board is not solved.
+        Update undo list with typing-a-digit action.
+        Set possible values for the filled cell as an empty list.
+        Removes previous value if existed.
+
+        :param key_press: a value of a key pressed. Only digits from 1 to 9 pass the conditions.
+        """
         if not self.is_solved:
             key_char = key_press.char
             if key_char.isdigit() and int(key_char) != 0:
@@ -167,6 +198,10 @@ class Board(Frame):
                 self.auto_switch()
 
     def change_checkbutton_label(self):
+        """
+        Turn auto-switch off and on.
+        Change button label according to settings.
+        """
         if self.auto_cell_switch.get():
             self.checkbox['text'] = 'Auto Cell Switch: OFF'
             self.is_auto_switching = False
@@ -175,6 +210,10 @@ class Board(Frame):
             self.is_auto_switching = True
 
     def auto_switch(self):
+        """
+        After typing a digit automatically jup to the next cell.
+        If current cell is utmost, jump to the first cell of the next row.
+        """
         if self.is_auto_switching:
             if self.current_cell.list_coord[1] == 8:
                 self.switch_cells_with_arrows('Right')
@@ -183,6 +222,11 @@ class Board(Frame):
                 self.switch_cells_with_arrows('Right')
 
     def delete_digit(self):
+        """
+        Remove a digit display from cell.
+        Change cell variables to that of an empty cell.
+        Update undo list with digit-removing action.
+        """
         if not self.is_solved:
             # update undo_list with the current action
             if self.current_cell.value is not None:
@@ -196,6 +240,14 @@ class Board(Frame):
         self.auto_switch()
 
     def switch_cells_with_arrows(self, arrow_press):
+        """
+        Enable switching cells with arrows. At utmost positions switch to first cell of next row.
+        Set new current cell after switch.
+        Enable auto-move directly through code.
+
+        :param arrow_press: can be a string representing arrow-key symbol while calling the method from code
+        or a key symbol if clicked by the user.
+        """
         # Adjust for auto_switch function:
         if isinstance(arrow_press, str):
             arrow = arrow_press
@@ -234,6 +286,14 @@ class Board(Frame):
         self.current_cell = new_current_cell
 
     def solve(self):
+        """
+        Assigned for solving button. Create an instance of SudokuSolver class which solves
+        the Board while initiated.
+        Add solving action to undo list.
+        Change Board status to "solved".
+        Show exception for invalid board which is raised by the Solver. If an exception occurs,
+        clear the board.
+        """
         # try:
         solver = SudokuSolver(self.cells, self.columns, self.squares)
         solver.solve()
@@ -248,6 +308,7 @@ class Board(Frame):
         #     self.clear_board()
 
     def undo(self):
+        """Depending on the type of action saved in the undo list, reset the state of the board."""
         if self.undo_list:
             last_move = self.undo_list[-1]
 
@@ -292,6 +353,7 @@ class Board(Frame):
             self.undo_list.pop(-1)
 
     def clear_board(self):
+        """Clear the board from solved digits (initially inserted digits remain) and update undo list."""
         self.is_solved = False
         is_cleared = False
 
@@ -306,6 +368,7 @@ class Board(Frame):
             self.undo_list.append(('clear',))
 
     def reset_board(self):
+        """Reset the Board to an empty state and update undo list."""
         self.is_solved = False
 
         # update undo_list with the current action:
